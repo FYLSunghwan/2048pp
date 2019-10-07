d13d37057ebf83258bb62f5e002904fabbcb2b1e
Sunghwan Kim <sunghwan519@hotmail.com>
2019-10-08 01:43:28 +0900

    feat: Implement 'Block.cc'

---
+++ Sources/G2048pp/Block.cc
@@ -0,0 +1,25 @@
+#include <Block.hpp>
+
+Block::Block(BlockState state)
+{
+    using Random = effolkronium::random_static;
+
+    m_state = state;
+    m_num = Random::get(1,2);
+}
+
+int Block::GetNum() const
+{
+    return m_num;
+}
+
+BlockState Block::GetState() const 
+{
+    return m_state;
+}
+
+Block& Block::operator+(const Block& rhs)
+{
+    m_num += rhs.GetNum();
+    m_state = rhs.GetState();
+}
